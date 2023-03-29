92c1c71843f5f74af527de51076e5927b5b15fda
dcodez23 <71770133+dcodez23@users.noreply.github.com>
2023-03-28 21:09:27 -0400

    Simple variable manipulation exercise

---
+++ Variables.py
@@ -0,0 +1,21 @@
+# 🚨 Don't change the code below 👇
+a = input("a: ")
+b = input("b: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+c = a 
+a = b
+b = c
+
+#Write your code above this line 👆
+####################################
+
+# 🚨 Don't change the code below 👇
+print("a: " + a)
+print("b: " + b)
+
+# purpose of this is code to make you think how you can switch around the values within a & b which in this example would require introducing a third to 
+#capture the value of a using c. Variables can be changed from one line to another ie: num = 1 then later in code block num = 3 
+#the value of num is now 3 but at first the value of num was = 1
